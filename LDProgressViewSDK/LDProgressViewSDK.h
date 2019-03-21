5455729995df0ab6e3b149ea8324ecf3b9ce7184
Diego Valenzuela <dvalenzu@gmail.com>
2019-03-21 08:05:45 -0700

    Update to use framework

---
+++ LDProgressViewSDK/LDProgressViewSDK.h
@@ -0,0 +1,21 @@
+//
+//  LDProgressViewSDK.h
+//  LDProgressViewSDK
+//
+//  Created by Diego Valenzuela on 3/21/19.
+//  Copyright © 2019 Light Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LDProgressViewSDK.
+FOUNDATION_EXPORT double LDProgressViewSDKVersionNumber;
+
+//! Project version string for LDProgressViewSDK.
+FOUNDATION_EXPORT const unsigned char LDProgressViewSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDProgressViewSDK/PublicHeader.h>
+
+
+#import <LDProgressViewSDK/LDProgressView.h>
+#import <LDProgressViewSDK/UIColor+RGBValues.h>
